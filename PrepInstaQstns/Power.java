e3d57cbbb566ae5719f59d6a0756560c0a0b2775
suvadityagupta <10suvaditya18@gmail.com>
2024-05-16 22:34:49 +0530

    Power.java

---
+++ PrepInstaQstns/Power.java
@@ -0,0 +1,19 @@
+package PrepInstaQstns;
+
+public class Power {
+   
+ 	public static void main(String[] args) {
+	    
+ 	double base = 1.5;
+     double expo1 = 2.5;
+     double expo2 = -2.5;
+     double res1, res2;
+    
+     // calculates the power
+     res1 = Math.pow(base, expo1);
+     res2 = Math.pow(base, expo2);
+ 	System.out.println(base + " ^ " + expo1 + " = " + res1 );
+ 	System.out.println(base + " ^ " + expo2 + " = " + res2 );
+ 	}
+ 
+}
